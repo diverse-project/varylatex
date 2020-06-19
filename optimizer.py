8f44cd87e75093808800ea3a1c081601740b73e4
Corentin <corentin.ollivier@free.fr>
2020-06-19 16:51:58 +0200

    Added a way to generate a random config from the possible values of the variables

--- optimizer.py
+++ optimizer.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import subprocess
+import random
 
 import fitz
 
@@ -84,6 +85,29 @@ def page_count(pdf_path):
     document = fitz.open(pdf_path)
     return document.pageCount
 
+def random_config(booleans = [], numbers = {}, enums = {}):
+    """
+    Generates a config dictionnary based on the range of values provided for each variable in the parameters.
+    Booleans are either true or false and only their names need to be specified.
+    Numbers are represented with the variable name as the key, and a tuple (min_val, max_val, precision) as the value.
+    Enums are variables that can have one value from a list provided as the value in the dictionnary.
+    """
+    config = {}
+    for boolean in booleans:
+        config["var-" + boolean] = random.choice([True, False])
+
+    for var_name, params in numbers.items():
+        min, max, precision = params
+        config["var-" + var_name] = round(random.uniform(min, max), precision)
+    
+    for var_name, choices in enums.items():
+        config["var-" + var_name] = random.choice(choices)
+
+    return config
+
+
+
+
 if __name__ == "__main__":
 
     base_path = os.getcwd()
@@ -92,21 +116,35 @@ if __name__ == "__main__":
     temp_path = create_temporary_copy(document_path)
 
     filename = "VaryingVariability-FSE15"
-    config = {
-        "var-PL_FOOTNOTE": True,
-        "var-ACK": True,
-        "var-PARAGRAPH_ACK": True,
-        "var-LONG_AFFILIATION": True,
-        "var-EMAIL": False,
-        "var-BOLD_ACK": False,
-        "var-LONG_ACK": False,
+#    config = {
+#        "var-PL_FOOTNOTE": True,
+#        "var-ACK": True,
+#        "var-PARAGRAPH_ACK": True,
+#        "var-LONG_AFFILIATION": True,
+#        "var-EMAIL": False,
+#        "var-BOLD_ACK": False,
+#        "var-LONG_ACK": False,
+#
+#        "var-vspace_bib": 4.32,
+#        "var-bref_size": 0.85,
+#        "var-cserver_size": 0.7,
+#
+#        "var-js_style": r"\tiny"
+#    }
+    config = random_config(
+        booleans = ["PL_FOOTNOTE", "ACK", "PARAGRAPH_ACK", "LONG_AFFILIATION", "EMAIL", "BOLD_ACK", "LONG_ACK"],
+        numbers = {
+            "vspace_bib": (1, 5, 2),
+            "bref_size": (0.7, 1, 2),
+            "cserver_size": (0.6, 0.9, 2)
+        },
+        enums = {
+            "js_style": [r"\tiny", r"\scriptsize", r"\footnotesize"]
+        }
+    )
+    # config = {'var-PL_FOOTNOTE': True, 'var-ACK': False, 'var-PARAGRAPH_ACK': False, 'var-LONG_AFFILIATION': True, 'var-EMAIL': False, 'var-BOLD_ACK': True, 'var-LONG_ACK': False, 'var-vspace_bib': 4.77, 'var-bref_size': 1.0, 'var-cserver_size': 0.63, 'var-js_style': '\\scriptsize'}
 
-        "var-vspace_bib": 4.32,
-        "var-bref_size": 0.85,
-        "var-cserver_size": 0.7,
-
-        "var-js_style": r"\tiny"
-    }
+    print(config)
 
     filename_tex = filename + ".tex"
     filename_pdf = filename + ".pdf"
@@ -118,3 +156,5 @@ if __name__ == "__main__":
     shutil.copyfile(os.path.join(temp_path, filename_pdf), os.path.join(base_path, filename_pdf))
 
     shutil.rmtree(temp_path)
+
+    
