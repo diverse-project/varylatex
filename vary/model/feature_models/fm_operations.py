0d970f9c0ca8a27031f04ef7def8641c94e7d3e7
jagalindo <jagalindo@us.es>
2021-08-20 01:49:39 +0200

    Introducing famapy to varylatex. Now it does generates, stil some weeoes in the analysis of the non-existent decision tree

---
+++ vary/model/feature_models/fm_operations.py
@@ -0,0 +1,93 @@
+from famapy.core.discover import DiscoverMetamodels # This loads the tool in the python execution eviroment
+import random
+
+def transcodeProducts(rawProducts):
+    transcoddedRes=[]
+    for result in rawProducts:
+        r={}
+        if "PL_FOOTNOTE" in result:
+            r["PL_FOOTNOTE"]=True
+        else:
+            r["PL_FOOTNOTE"]=False
+        
+        if "ACK" in result:
+            r["ACK"]=True
+        else:
+            r["ACK"]=False
+        
+        if "LONG_AFFILIATION" in result:
+            r["LONG_AFFILIATION"]=True
+        else:
+            r["LONG_AFFILIATION"]=False
+        
+        if "EMAIL" in result:
+            r["EMAIL"]=True
+        else:
+            r["EMAIL"]=False
+        
+        if "LONG_ACK" in result:
+            r["LONG_ACK"]=True
+        else:
+            r["LONG_ACK"]=False
+        
+        if "vspace_UNO" in result:
+            r["vspace"]=1
+        elif "vspace_CINCO" in result:
+            r["vspace"]=5
+        elif "vspace_DOS" in result:
+            r["vspace"]=2
+        else:
+            raise Exception
+        
+        if "bref_size_CERO_SIETE" in result:
+            r["bref_size"]=0.7
+        elif "bref_size_UNO" in result:
+            r["bref_size"]=1
+        elif "bref_size_DOS" in result:
+            r["bref_size"]=2
+        else:
+            raise Exception
+        
+        if "cserver_size_CERO_SEIS" in result:
+            r["cserver_size"]=0.6
+        elif "cserver_size_CERO_NUEVE" in result:
+            r["cserver_size"]=0.9
+        elif "cserver_size_DOS" in result:
+            r["cserver_size"]=2
+        else:
+            raise Exception
+        
+        if "tiny" in result:
+            r["js_style"]="\\tiny"
+        elif "scriptsize" in result:
+            r["js_style"]="\\scriptsize"
+        elif "footnotesize" in result:
+            r["js_style"]="\\footnotesize"
+
+        if "PARAGRAPH_ACK" in result:
+            r["PARAGRAPH_ACK"]=True
+        else:
+            r["PARAGRAPH_ACK"]=False
+
+        if "BOLD_ACK" in result:
+            r["BOLD_ACK"]=True
+        else:
+            r["BOLD_ACK"]=False
+
+        transcoddedRes.append(r)
+    return transcoddedRes
+
+def getProducts(path):
+    dm = DiscoverMetamodels()
+    results = dm.use_operation_from_file("Products", path) # This launch the operation and stores the result on the result variable
+    results = transcodeProducts(results)
+    return results
+
+
+def getRandomProduct(products):
+    n = random.randint(0,len(products)-1)
+    return products[n]
+
+def getFeatureHeaders(path):
+    #This is still hardcoded for the example. TODO See how to generalize
+    return ["PL_FOOTNOTE", "ACK", "LONG_AFFILIATION", "EMAIL", "LONG_ACK", "vspace_bib", "bref_size", "cserver_size", "js_style", "PARAGRAPH_ACK", "BOLD_ACK", "nbPages", "space"]
